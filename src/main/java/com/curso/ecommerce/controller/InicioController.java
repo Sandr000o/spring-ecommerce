c805956c0a675e093b62a1557aa1862a2dd095b4
Sandr000o <lisandro4842@gmail.com>
2025-03-26 14:53:17 -0500

    Vista para el usuario del producto seleccionado

--- src/main/java/com/curso/ecommerce/controller/InicioController.java
+++ src/main/java/com/curso/ecommerce/controller/InicioController.java
@@ -2,10 +2,13 @@ package com.curso.ecommerce.controller;
 
 import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -14,6 +17,8 @@ import java.util.List;
 @RequestMapping("/")
 public class InicioController {
 
+    private Logger LOGGER = LoggerFactory.getLogger(InicioController.class);
+
     @Autowired
     private ProductoService productoService;
     @GetMapping("")
@@ -21,5 +26,14 @@ public class InicioController {
         List<Producto> listaProductos=productoService.getProductos();
         model.addAttribute("listaProductos", listaProductos);
         return "usuario/home";
+    }
+
+    @GetMapping("productohome/{id}")
+    public String verProducto(@PathVariable Integer id, Model model) {
+        LOGGER.info("Id del producto: " + id);
+        Producto producto= productoService.getProductoById(id).orElse(null);
+        model.addAttribute("producto", producto);
+        LOGGER.info("Producto: " + producto);
+        return "usuario/productohome";
     }
 }
