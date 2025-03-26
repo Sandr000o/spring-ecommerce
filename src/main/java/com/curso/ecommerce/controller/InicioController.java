60815aab370646bb686a2afbb4d969ffd021d457
Sandr000o <lisandro4842@gmail.com>
2025-03-26 17:04:04 -0500

    Redirreción al carrito de comprar al seleccionar un producto

--- src/main/java/com/curso/ecommerce/controller/InicioController.java
+++ src/main/java/com/curso/ecommerce/controller/InicioController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -38,5 +39,11 @@ public class InicioController {
         model.addAttribute("producto", producto);
         LOGGER.info("Producto: " + producto);
         return "usuario/productohome";
+    }
+
+
+    @PostMapping("/cart")
+    public String addCart(){
+        return "usuario/carrito";
     }
 }
