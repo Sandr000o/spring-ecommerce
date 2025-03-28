4d980a00add93bed295e0f277d1c13b06e5a3de1
Sandr000o <lisandro4842@gmail.com>
2025-03-28 16:40:04 -0500

    Método para generar el número de orden

--- src/main/java/com/curso/ecommerce/service/OrdenServiceImpl.java
+++ src/main/java/com/curso/ecommerce/service/OrdenServiceImpl.java
@@ -5,6 +5,9 @@ import com.curso.ecommerce.repository.IOrdenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class OrdenServiceImpl implements IOrdenService {
 
@@ -12,7 +15,34 @@ public class OrdenServiceImpl implements IOrdenService {
     private IOrdenRepository ordenRepository;
 
     @Override
+    public List<Orden> obtenerOrdenes() {
+        return ordenRepository.findAll();
+    }
+
+    @Override
     public Orden guardarOrden(Orden orden) {
         return ordenRepository.save(orden);
     }
+
+
+    public String generarNumeroOrden() {
+        int numeroOrden = 0;
+        String numeroConcatenado = "";
+        List<Orden> ordenes = obtenerOrdenes();
+        List<Integer> numeroCorrelativo = new ArrayList<Integer>();
+        ordenes.stream().forEach(orden ->
+                numeroCorrelativo.add(Integer.parseInt(orden.getNumero())));
+
+        if (ordenes.isEmpty()) {
+            numeroOrden=1;
+        }else{
+            numeroOrden=numeroCorrelativo.stream().max(Integer::compare).get();
+            numeroOrden++;
+        }
+
+        numeroConcatenado = String.format("%09d", numeroOrden);
+
+        return numeroConcatenado;
+    }
+
 }
