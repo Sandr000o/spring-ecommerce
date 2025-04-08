9082413818a6dae0937e6c508bfa12d36652cdc8
Sandr000o <lisandro4842@gmail.com>
2025-04-08 14:11:58 -0500

    Funcionalidad para ver las ordenes con el usuairo ADMIN

--- src/main/java/com/curso/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/curso/ecommerce/controller/AdministradorController.java
@@ -1,7 +1,9 @@
 package com.curso.ecommerce.controller;
 
+import com.curso.ecommerce.model.Orden;
 import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.model.Usuario;
+import com.curso.ecommerce.service.IOrdenService;
 import com.curso.ecommerce.service.IProductoService;
 import com.curso.ecommerce.service.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,9 @@ public class AdministradorController {
     @Autowired
     private IUsuarioService IUsuarioService;
 
+    @Autowired
+    private IOrdenService IOrdenService;
+
     @GetMapping("")
     public String home(Model model) {
         List<Producto> listaProductos = IProductoService.getProductos();
@@ -35,6 +40,13 @@ public class AdministradorController {
         List<Usuario> listaUsuarios = IUsuarioService.findAllUsuarios();
         model.addAttribute("listaUsuarios", listaUsuarios);
         return "administrador/usuarios";
+    }
+
+    @GetMapping("/ordenes")
+    public String ordenes(Model model) {
+        List<Orden> ordenes = IOrdenService.obtenerOrdenes();
+        model.addAttribute("listaOrdenes", ordenes);
+        return "administrador/ordenes";
     }
 
 }
